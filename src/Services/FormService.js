3eac67ae0cefc4687e676ea92129e9106c65ac3f
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2023-02-23 15:20:54 +0300

    forth commit

--- src/Services/FormService.js
+++ src/Services/FormService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class FormService {
   postPreRegister(form) {
-    return axios.post("https://mely-back-production.up.railway.app:6356/api/form/register",form);
+    return axios.get("https://mely-back-production.up.railway.app/api/form/register3");
   }
   postPreRegister2(file) {
     return axios.post("http://localhost:8080/api/form/register2",file);
